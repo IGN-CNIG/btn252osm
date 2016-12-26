f5cad464beefc3453663e5830061b1ab9541cb62
Santiago Crespo <github.com@flanera.net>
2016-12-26 16:34:05 +0100

    Create SENDA.py

---
+++ translations/SENDA.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+def filterTags(attrs):
+	if not attrs: return
+
+	tags={}
+	tags.update({'highway':'track'})
+	tags.update({'surface':'compacted'})
+	tags.update({'tracktype':'grade1'})
+
+# Nombre
+	tags.update({'name':'x'})
+	if attrs['ETIQUETA'] == "":
+		del tags['name']
+	else:
+		tags ['name'] = attrs ['ETIQUETA']
+
+# Situación SITUA_0610 (subterránea, superficial, elevada)
+	tags.update({'layer':'x'})
+	if attrs['SITUA_0610'] == '01':
+		tags.update({'layer':'-1'})
+	elif attrs['SITUA_0610'] == '02':
+                del tags['layer']
+#		tags.update({'layer':'0'})
+	elif attrs['SITUA_0610'] == '03':
+		tags.update({'layer':'1'})
+
+	return tags
